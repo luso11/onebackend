633882f6083720536e7f27c3fd8a5804083a9fc1
Luis Alonso <luisluso89@gmail.com>
2019-03-25 10:57:32 +0100

    Update bashScript.sh

--- bashScript.sh
+++ bashScript.sh
@@ -4,21 +4,23 @@ group=$(groups)
 
 for folder in "$@"
 do
-	directories=$(ls -l $folder | grep ^d | awk -v user="$user" -v group="$group"\
-	'($3==user && match($1,/^.r.x/)) || ($4==group && match($1,/^....r.x/)) || match($1,/^.......r.x/) {print $9}')
-	echo "Already have permissions on: " $directories
-	lista=[]
-	for subdirectory in $directories
-	do
-		subdirectoriesAndFiles=$(ls -l $folder/$subdirectory | awk -v user="$user" -v group="$group" \
-		'($3==user && match($1,/^.r.x/)) || ($4==group && match($1,/^....r.x/)) || match($1,/^.......r.x/) {print $9}')
-	done
-	echo "Already have permissions on these subdirectories and files: " $subdirectoriesAndFiles
-	files=$(ls -l $folder | grep -v ^d | awk -v user="$user" -v group="$group"\
+        echo "Already have permissions on these directories: "
+        directories=$(ls -l $folder | grep ^d | awk -v user="$user" -v group="$group"\
+        '($3==user && match($1,/^.r.x/)) || ($4==group && match($1,/^....r.x/)) || match($1,/^.......r.x/) {print $9}')
+        echo $directories
+        echo "Already have permissions on these subdirectories and files: "
+        for subdirectory in $directories
+        do
+                echo "Directory: " $subdirectory
+                echo "File: " $(ls -l $folder/$subdirectory | awk -v user="$user" -v group="$group" \
+                '($3==user && match($1,/^.r.x/)) || ($4==group && match($1,/^....r.x/)) || match($1,/^.......r.x/) {print $9}')
+        done
+        files=$(ls -l $folder | grep -v ^d | awk -v user="$user" -v group="$group"\
         '!($3==user && match($1,/^.r.x/)) && !($4==group && match($1,/^....r.x/)) && !match($1,/^.......r.x/) {print $9}')
-	for file in $files
-	do
-		 chmod u+rx $folder/$file
-	done
+        for file in $files
+        do
+                echo "Changing permissions to file: " $folder$file
+                sudo chmod +rx $folder/$file
+        done
 
 done
